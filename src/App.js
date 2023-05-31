100ade83434fc6d51dea16d969e526cf56432417
Lena Yoo <lenayoo17@gmail.com>
2023-05-31 12:35:13 +0900

    finish structure

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
-import logo from './logo.svg'
 import './App.css'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">LenaDiary</header>
+    <div className='App'>
+      <h2>Lena Diary</h2>
+      <div className="body">
+        <input className="writer" />
+        <textarea className="diary-content"></textarea>
+        <select className="emotion">
+          <option>1</option>
+          <option>2</option>
+          <option>3</option>
+          <option>4</option>
+          <option>5</option>
+        </select>
+      </div>
+      <div className='line'></div>
     </div>
   )
 }
