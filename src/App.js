a6bdfd8f7e7a26ec08109ee10175e45de8cb7e8d
Lena Yoo <lenayoo17@gmail.com>
2023-06-17 11:57:14 +0900

    add diaries map function

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './App.css'
 import { useState } from 'react'
 
 function App() {
+  const [diaries, setDiaries] = useState([])
   const [diary, setDiary] = useState({
     title: '',
     text: '',
@@ -13,14 +14,13 @@ function App() {
       ...diary,
       [e.target.name]: e.target.value,
     })
-
-    console.log(e.target.name)
-    console.log(e.target.value)
   }
 
   function submitHandler(e) {
     e.preventDefault()
     console.log(diary)
+    setDiaries([...diaries, diary])
+    setDiary({ title: '', text: '', emotion: 1 })
     alert('오늘의 일기를 저장했습니다.')
   }
 
@@ -49,15 +49,23 @@ function App() {
           name="emotion"
           value={diary.emotion}
         >
-          <option>1</option>
-          <option>2</option>
-          <option>3</option>
-          <option>4</option>
-          <option>5</option>
+          <option>매우좋음</option>
+          <option>완전 좋음</option>
+          <option>꽤 좋음</option>
+          <option>Perfect</option>
+          <option>성장의 날</option>
         </select>
         <button type="submit">submit</button>
       </form>
       <div className="line"></div>
+      {diaries.map((diary, index) => (
+        <div key={index} className="diary-list">
+          <h2 className="diary-title">{diary.title}</h2>
+          <div className="diary-line"></div>
+          <p className="diary-content">{diary.text}</p>
+          <p className="diary-emotion">{diary.emotion}</p>
+        </div>
+      ))}
     </div>
   )
 }
